c77334bcf58337e6a6f611e2491483a55e7b8ad1
William Herring <73984595+william-herring@users.noreply.github.com>
2022-09-22 07:37:45 +1000

    Added GitHub link

--- pages/index.tsx
+++ pages/index.tsx
@@ -36,7 +36,11 @@ const Home: NextPage = () => {
           </div>
 
           <div className='fixed p-4 h-1/3 bottom-0 w-screen'>
-              <svg className='fixed bottom-0 right-0 m-4' width="26px" height="26px" viewBox="0 0 24 24" fill="none"><path d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>
+              <Link href='https://github.com/william-herring/eventron'>
+                  <a>
+                      <svg className='fixed bottom-0 right-0 m-4' width="26px" height="26px" viewBox="0 0 24 24" fill="none"><path d="M16 22.027v-2.87a3.37 3.37 0 00-.94-2.61c3.14-.35 6.44-1.54 6.44-7a5.44 5.44 0 00-1.5-3.75 5.07 5.07 0 00-.09-3.77s-1.18-.35-3.91 1.48a13.38 13.38 0 00-7 0c-2.73-1.83-3.91-1.48-3.91-1.48A5.07 5.07 0 005 5.797a5.44 5.44 0 00-1.5 3.78c0 5.42 3.3 6.61 6.44 7a3.37 3.37 0 00-.94 2.58v2.87M9 20.027c-3 .973-5.5 0-7-3" stroke="#000000" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/></svg>
+                  </a>
+              </Link>
           </div>
       </div>
   )
