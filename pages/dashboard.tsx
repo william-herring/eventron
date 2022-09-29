33d71f49040ac78bc66816f4f3c85451e0275923
William Herring <73984595+william-herring@users.noreply.github.com>
2022-09-29 15:32:43 +1000

    Redirect from dashboard page if unauthenticated

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -2,10 +2,26 @@ import { NextPage } from "next"
 import { signOut, useSession } from "next-auth/react"
 import Head from "next/head"
 import Image from "next/image"
+import { useRouter } from "next/router"
 import NavigationBar from "../components/NavigationBar"
 
 const Dashboard: NextPage = () => {
     const { data: session, status } = useSession()
+    const router = useRouter()
+
+    if (status === "loading") {
+        return <div className='flex w-screen h-screen justify-center items-center'>
+                <Image width={80} height={80} src='/loading_spinner.svg' className='animate-spin' />
+            </div>
+    }
+
+    if (status === "unauthenticated") {
+        router.replace('/')
+    }
+
+    if (status != "authenticated") {
+        return <div></div>
+    }
   
     return (
         <div>
