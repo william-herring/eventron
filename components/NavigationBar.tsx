4b5a6e8f9788f854e4ba8e7519713fce12b7cba4
William Herring <73984595+william-herring@users.noreply.github.com>
2022-10-03 07:23:28 +1100

    Added user link to navigation bar

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react"
 import Link from "next/link"
 import React from "react"
 
@@ -6,6 +7,8 @@ interface NavigationBarProps {
 }
 
 const NavigationBar: React.FC<NavigationBarProps> = (props) => {
+    const { data: session } = useSession()
+
     return <div className='flex w-screen fixed p-4'>
         <div className="flex space-x-8">
             <Link href='#'>
@@ -24,10 +27,11 @@ const NavigationBar: React.FC<NavigationBarProps> = (props) => {
                 </a>
             </Link>
         </div>
-
-        <button className='ml-auto'>
-            <svg width="38px" height="38px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2z" stroke="#1d4ed8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4.271 18.346S6.5 15.5 12 15.5s7.73 2.846 7.73 2.846M12 12a3 3 0 100-6 3 3 0 000 6z" stroke="#1d4ed8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
-        </button>
+        <Link href={`/user/${session?.user?.name}`}>
+            <a className='ml-auto'>
+                <svg width="38px" height="38px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#000000"><path d="M12 2C6.477 2 2 6.477 2 12s4.477 10 10 10 10-4.477 10-10S17.523 2 12 2z" stroke="#1d4ed8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path><path d="M4.271 18.346S6.5 15.5 12 15.5s7.73 2.846 7.73 2.846M12 12a3 3 0 100-6 3 3 0 000 6z" stroke="#1d4ed8" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
+            </a>
+        </Link>
     </div>
 }
 
