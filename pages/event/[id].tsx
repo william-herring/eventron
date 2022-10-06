a6b917439f69bff5abfad74181f6aa7e9dbd8877
William Herring <73984595+william-herring@users.noreply.github.com>
2022-10-07 07:41:02 +1100

    incredibly minor commit with width change

--- pages/event/[id].tsx
+++ pages/event/[id].tsx
@@ -32,13 +32,13 @@ const Account: NextPage<{ event: any | null }> = (props) => {
     const [ tab, setTab ] = useState(0)
 
     return (
-        <div>
+        <div className='overflow-hidden'>
             <Head>
                 <title>{props.event?.title}</title>
             </Head>
             <NavigationBar active={5} />
-            <div className='flex flex-col bottom-0 items-center justify-center h-screen w-screen bg-gray-100'>
-                <div className='rounded-2xl p-12 shadow-lg bg-white'>
+            <div className='flex flex-col bottom-0 items-center justify-center h-screen bg-gray-100'>
+                <div className='rounded-2xl p-12 shadow-lg bg-white w-96'>
                     <div className='flex items-center'>
                         <svg width="20px" height="20px" stroke-width="2" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" color="#6b7280"><path d="M1 20v-1a7 7 0 017-7v0a7 7 0 017 7v1" stroke="#6b7280" stroke-width="2" stroke-linecap="round"></path><path d="M13 14v0a5 5 0 015-5v0a5 5 0 015 5v.5" stroke="#6b7280" stroke-width="2" stroke-linecap="round"></path><path d="M8 12a4 4 0 100-8 4 4 0 000 8zM18 9a3 3 0 100-6 3 3 0 000 6z" stroke="#6b7280" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"></path></svg>
                         <p className='text-gray-500 ml-2 text-sm'>{props.event.community != undefined? props.event.community.title : ''}</p>
